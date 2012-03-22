e8d9a55cc02409f2d967ec6c18f0e64296a9d8fb
Kaffeemon <kaffeemon@aol.de>
2012-03-22 02:00:48 +0100

    added permissions to BoxAction and fixed users online box template

--- file/lib/data/box/BoxAction.class.php
+++ file/lib/data/box/BoxAction.class.php
@@ -12,5 +12,26 @@ class BoxAction extends \wcf\data\AbstractDatabaseObjectAction {
 	 * @see \wcf\data\AbstractDatabaseObjectAction::$className
 	 */
 	protected $className = '\wcf\data\box\BoxEditor';
+	
+	/**
+	 * @see \wcf\data\AbstractDatabaseObjectAction::$permissionsCreate
+	 */
+	protected $permissionsCreate = array(
+		'admin.content.box.canAdd'
+	);
+	
+	/**
+	 * @see \wcf\data\AbstractDatabaseObjectAction::$permissionsDelete
+	 */
+	protected $permissionsDelete = array(
+		'admin.content.box.canDelete'
+	);
+	
+	/**
+	 * @see \wcf\data\AbstractDatabaseObjectAction::$permissionsUpdate
+	 */
+	protected $permissionsUpdate = array(
+		'admin.content.box.canEdit'
+	);
 }
 
--- template/usersOnlineBoxType.tpl
+++ template/usersOnlineBoxType.tpl
@@ -1,7 +1,7 @@
 <h3>{lang}wcf.box.type.usersOnline.info{/lang}</h3>
 
 <p>{implode from=$box->boxCache.usersOnline item=$user}
-	<a href="{link controller='User' id=$users.userID}{/link}">
+	<a href="{link controller='User' id=$user.userID}{/link}">
 		{$user.username}
 	</a>
 {/implode}</p>
