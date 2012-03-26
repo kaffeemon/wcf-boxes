5a7ade67351cec1321fff81b3b61aeeb1dc18e88
Kaffeemon <kaffeemon@aol.de>
2012-03-26 15:05:29 +0300

    added options enableTPLCode and enableHTML to staticBoxType

--- file/lib/system/box/type/StaticBoxType.class.php
+++ file/lib/system/box/type/StaticBoxType.class.php
@@ -17,7 +17,17 @@ class StaticBoxType extends \wcf\system\box\AbstractBoxType {
 			new \wcf\data\option\Option(null, array(
 				'optionName' => 'content',
 				'optionType' => 'textarea'
-			))
+			)),
+			
+			new \wcf\data\option\Option(null, array(
+				'optionName' => 'enableHTML',
+				'optionType' => 'boolean'
+			)),
+			
+			new \wcf\data\option\Option(null, array(
+				'optionName' => 'enableTPLCode',
+				'optionType' => 'boolean'
+			)),
 		);
 	}
 	
@@ -25,7 +35,17 @@ class StaticBoxType extends \wcf\system\box\AbstractBoxType {
 	 * @see \wcf\system\box\IBoxType::render()
 	 */
 	public function render() {
-		return WCF::getTPL()->fetchString($this->content);
+		$content = $this->content;
+		
+		// tpl code
+		if ($this->enableTPLCode)
+			$content = WCF::getTPL()->fetchString($content);
+		
+		// html
+		if (!$this->enableHTML)
+			$content = \wcf\util\StringUtil::encodeHTML($content);
+		
+		return $content;
 	}
 }
 
